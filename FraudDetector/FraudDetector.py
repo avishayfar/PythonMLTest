75d85e1bf6537b22bc2de1ae312fecec1845ab5b
Avishay Farbstein <avish.far@gmail.com>
2018-11-11 19:02:36 +0200

    change the build of x and y

--- FraudDetector/FraudDetector.py
+++ FraudDetector/FraudDetector.py
@@ -36,12 +36,15 @@ def RunFraudDetector(basicDataTableName, storeWithHierarchyName, algorithmName, 
     #query = 'select * from "{}"'.format(basicDataTableName)   
     #dfAll =  pd.read_sql_query(query, con=engine)
 
-    tempPath = "C:\\SeScFRD\\Input\\ItamarData.xlsx"
+    #tempPath = "C:\\SeScFRD\\Input\\ItamarData.xlsx"
+    tempPath = "C:\\SeScFRD\\Input\\dataWithHierarchies.xlsx"
     dfAll = pd.read_excel(tempPath)
 
-    namesOfColumn4Learning.append(rescanResultColumnName)
-    namesOfColumn4Learning.append(shrinkageColumnName)
-    df = dfAll[namesOfColumn4Learning]
+
+    namesOfAllColumns = namesOfColumn4Learning.copy()
+    namesOfAllColumns.append(rescanResultColumnName)
+    namesOfAllColumns.append(shrinkageColumnName)
+    df = dfAll[namesOfAllColumns]
 
     dfResult_1 = df[df[rescanResultColumnName] == 1]
     dfResult_0 = df[df[rescanResultColumnName] == 0]
@@ -58,8 +61,10 @@ def RunFraudDetector(basicDataTableName, storeWithHierarchyName, algorithmName, 
         dfForBuildModel_1, dfFinalTest_1 = train_test_split(dfResult_1, test_size=testSize)
         dfForBuildModel_0, dfFinalTest_0 = train_test_split(dfResult_0, test_size=testSize)
 
-        dfForBuildModel = dfForBuildModel_1.append(dfForBuildModel_0)
-        dfFinalTest = dfFinalTest_1.append(dfFinalTest_0)
+        #dfForBuildModel = dfForBuildModel_1.append(dfForBuildModel_0)
+        dfForBuildModel = pd.concat([dfForBuildModel_1,dfForBuildModel_0])
+        #dfFinalTest = dfFinalTest_1.append(dfFinalTest_0)
+        dfFinalTest = pd.concat([dfFinalTest_1,dfFinalTest_0])
 
         if(namesOfColumn4Normliaze != ""):
             NormlizeColumnsValue(dfForBuildModel, dfFinalTest,namesOfColumn4Normliaze,3)
@@ -74,10 +79,8 @@ def RunFraudDetector(basicDataTableName, storeWithHierarchyName, algorithmName, 
 
         # Build data for the machine learning 
 
-        numberOfColumns = dfForBuildModel.shape[1]
-        array = dfForBuildModel.values
-        dfForBuildModel_x = array[:,0:numberOfColumns -2]
-        dfForBuildModel_y = array[:,numberOfColumns - 2]
+        dfForBuildModel_x = dfForBuildModel[namesOfColumn4Learning]
+        dfForBuildModel_y = dfForBuildModel[rescanResultColumnName]
            
         #Start machine learning.
 
@@ -88,10 +91,8 @@ def RunFraudDetector(basicDataTableName, storeWithHierarchyName, algorithmName, 
 
         #Predict
 
-        columnsOnlyX = dfFinalTest.columns[0:numberOfColumns-2]
-        dfOnlyX = dfFinalTest[columnsOnlyX]
+        dfOnlyX = dfFinalTest[namesOfColumn4Learning]
         dfFinalTest["Predict"] = model.predict_proba(dfOnlyX)[:,0]  
-
 
         #Get specific data information
 
@@ -147,6 +148,121 @@ def GetRecallPrecision(dfWithScanResult, rescanResultColumnName, numberOfRealFra
           recallPrcesion = num/len*100
           return recallPrcesion;
    return 100;
+
+############################################
+
+#def RunFraudDetector(basicDataTableName, storeWithHierarchyName, algorithmName, namesOfColumn4Learning, namesOfColumn4Normliaze, rescanResultColumnName, shrinkageColumnName, times, threshold, OriginRescanRate):
+
+#    #engine = create_engine('postgresql://postgres:Qwe12345@localhost:5432/FRD')
+#    #query = 'select * from "{}"'.format(basicDataTableName)   
+#    #dfAll =  pd.read_sql_query(query, con=engine)
+
+#    #tempPath = "C:\\SeScFRD\\Input\\ItamarData.xlsx"
+#    tempPath = "C:\\SeScFRD\\Input\\dataWithHierarchies.xlsx"
+#    dfAll = pd.read_excel(tempPath)
+
+
+#    namesOfAllColumns = namesOfColumn4Learning.copy()
+#    namesOfAllColumns.append(rescanResultColumnName)
+#    namesOfAllColumns.append(shrinkageColumnName)
+#    df = dfAll[namesOfAllColumns]
+
+#    dfResult_1 = df[df[rescanResultColumnName] == 1]
+#    dfResult_0 = df[df[rescanResultColumnName] == 0]
+
+#    OriginalFraudPrecision = dfResult_1.shape[0]/df.shape[0]
+
+#    shrinkageSum = df[shrinkageColumnName].sum()
+#    originalShrinkagePerFruadLine = shrinkageSum/(df.shape[0])
+
+#    numberOfTruePredictList = []
+#    precisionOfLines4RecallList = []
+#    precsionOfShrinkage4LineList = []
+#    for num in range(0,times):
+#        dfForBuildModel_1, dfFinalTest_1 = train_test_split(dfResult_1, test_size=testSize)
+#        dfForBuildModel_0, dfFinalTest_0 = train_test_split(dfResult_0, test_size=testSize)
+
+#        dfForBuildModel = dfForBuildModel_1.append(dfForBuildModel_0)
+#        dfFinalTest = dfFinalTest_1.append(dfFinalTest_0)
+
+#        if(namesOfColumn4Normliaze != ""):
+#            NormlizeColumnsValue(dfForBuildModel, dfFinalTest,namesOfColumn4Normliaze,3)
+    
+#        #print data
+#        print()
+#        print(".....................................")
+#        print("All data size - ", df.shape)
+#        print("The precsion of fraud - ", OriginalFraudPrecision)
+#        print("dfForBuildModel size - ", dfForBuildModel.shape)
+#        print("dfFinalTest size - ", dfFinalTest.shape)
+
+#        # Build data for the machine learning 
+
+#        #numberOfColumns = dfForBuildModel.shape[1]
+#        #array = dfForBuildModel.values
+#        #dfForBuildModel_x = array[:,0:numberOfColumns -2]
+#        #dfForBuildModel_y = array[:,numberOfColumns - 2]
+
+#        dfForBuildModel_x = dfForBuildModel[namesOfColumn4Learning]
+#        dfForBuildModel_y = dfForBuildModel[rescanResultColumnName]
+           
+#        #Start machine learning.
+
+#        model = GetMLModel(algorithmName)
+#        model.fit(dfForBuildModel_x, dfForBuildModel_y.astype(int))
+
+#        print("model -", algorithmName)
+
+#        #Predict
+
+#        columnsOnlyX = dfFinalTest.columns[0:numberOfColumns-2]
+#        dfOnlyX = dfFinalTest[columnsOnlyX]
+#        dfFinalTest["Predict"] = model.predict_proba(dfOnlyX)[:,0]  
+
+#        #Get specific data information
+
+#        numberOfAllRowsFinalTest = dfFinalTest.shape[0] 
+#        numberOfRows4Anlayze = int(numberOfAllRowsFinalTest * OriginRescanRate/100)
+
+#        dfFinalTest = dfFinalTest.sort_values(by="Predict",ascending = True)
+#        df4Analayze = dfFinalTest.head(numberOfRows4Anlayze)
+#        numberOfTruePredict = (df4Analayze[rescanResultColumnName][df4Analayze[rescanResultColumnName] > 0]).sum()
+#        precsionOfShrinkage4Line = df4Analayze[shrinkageColumnName].sum()/numberOfRows4Anlayze 
+#        precisionOfTruePredict = numberOfTruePredict/df4Analayze.shape[0]*100
+
+#        numberOfRealFraudLinesToBeCatch = round(OriginalFraudPrecision * numberOfRows4Anlayze) 
+#        precisionOfRecall4GetPreDefinedFraud = GetRecallPrecision(dfFinalTest, rescanResultColumnName, numberOfRealFraudLinesToBeCatch)
+
+#        print("numberOfAllRowsFinalTest - ", numberOfAllRowsFinalTest)
+#        print("numberOfRows4Anlayze - ", numberOfRows4Anlayze)
+#        print("precisionOfTruePredict - ", precisionOfTruePredict) 
+#        print("precsionOfShrinkage4Line - ", precsionOfShrinkage4Line)
+#        print("precisionOfRecall4GetPreDefinedFraud - ", precisionOfRecall4GetPreDefinedFraud)
+#        print(".......................................")
+#        print()
+
+#        numberOfTruePredictList.append(precisionOfTruePredict)
+#        precisionOfLines4RecallList.append(precisionOfRecall4GetPreDefinedFraud)
+#        precsionOfShrinkage4LineList.append(precsionOfShrinkage4Line)
+
+#    numberOfTruePredictNdList = np.array(numberOfTruePredictList)
+#    precisionOfRecalLineslNdList = np.array(precisionOfLines4RecallList)
+#    precsionOfShrinkage4LineNdList = np.array(precsionOfShrinkage4LineList)
+
+#    FraudResults = {}
+#    FraudResults["Model"] = algorithmName
+#    FraudResults["Times"] = times
+#    FraudResults["PrecsionOfOriginalFraud"] = OriginalFraudPrecision*100
+#    FraudResults["PrecsionOfTruePredict"] = numberOfTruePredictNdList.mean()
+#    #FraudResults["PrecsionOfTruePredictStd"] = numberOfTruePredictNdList.std()
+#    FraudResults["OriginalPrecisionOfLinesForRecall"] = OriginRescanRate
+#    FraudResults["PrecisionOfLinesForRecall"] = precisionOfRecalLineslNdList.mean()
+#    #FraudResults["NumberOfLinesForRecallStd"] = precisionOfRecalLineslNdList.std()
+#    FraudResults["OriginalShrinkagePerLine"] = originalShrinkagePerFruadLine
+#    FraudResults["ShrinkagePerLine"] = precsionOfShrinkage4LineNdList.mean()
+
+#    return FraudResults
+
 
 
    
--- FraudDetector/PlayGround.py
+++ FraudDetector/PlayGround.py
@@ -1,16 +1,16 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-ResultsDf = pd.DataFrame(columns=['NumberOfItems','NumberOfVoidedItems', 'RescanResult'])
-for i in range(0, 1000):
-    NumberOfItems = i
-    NumberOfVoidedItems = i
-    RescanResult = 1 if i>500 else 0
-    ResultsDf.loc[i] = [NumberOfItems, NumberOfVoidedItems, RescanResult]
+tempPath = "C:\\SeScFRD\\Input\\ItamarData.xlsx"
+df = pd.read_excel(tempPath)
 
-#Save the results to file  
-outputPath = "C:\\SeScFRD\\Input\\avishayData.xlsx" 
-ew = pd.ExcelWriter(outputPath)
-ResultsDf.to_excel(ew, sheet_name='data')
-ew.save()
+df.plot.scatter(x='TotalShrinkage',y='RescanResult',c='red',s=5)
+plt.show()
+
+##Save the results to file  
+#outputPath = "C:\\SeScFRD\\Input\\avishayData.xlsx" 
+#ew = pd.ExcelWriter(outputPath)
+#ResultsDf.to_excel(ew, sheet_name='data')
+#ew.save()
